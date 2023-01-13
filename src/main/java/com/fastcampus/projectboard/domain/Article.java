62ec1c8b82183ccd51cf124b67b5e131a36b43a3
Gagasbandas <kjjcds@gmail.com>
2023-01-13 15:26:33 +0900

    #13 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스 == 1 테이블 매칭되도록 설계했다.
    생성자와 'equals()', 'hashcode()' 등이 도메인 맥락에 맞게끔 디테일하게 설정함.

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -1,15 +1,72 @@
 package com.fastcampus.projectboard.domain;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 게시글 번호
-    private String title; // 게시글 제목
-    private String content; // 게시글 내용
-    private String hashtag; // 게시글 해시태그
 
-    private LocalDateTime createdAt; // 게시글 작성일
-    private LocalDateTime modifiedAt; // 게시글 수정일
-    private String createdBy; // 게시글 작성자
-    private String modifiedBy; // 게시글 수정자
+    @Setter @Column(nullable = false, length = 255) private String title; // 게시글 제목
+    @Setter @Column(nullable = false, length = 10000) private String content; // 게시글 내용
+
+    @Setter private String hashtag; // 게시글 해시태그
+
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 게시글 작성일
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 게시글 작성자
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 게시글 수정일
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 게시글 수정자
+
+    protected Article() {}
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id != null && id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
