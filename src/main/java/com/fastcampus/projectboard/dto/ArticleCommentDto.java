b0cf15585a045b89765bc318061eb007f8571be1
Gagasbandas <kjjcds@gmail.com>
2023-02-17 17:57:23 +0900

    #62 - 대댓글 비즈니스 로직 구현
    부모/자식 관계를 만들면서 entity <-> dto 간 변환 로직.
    게시글을 db로부터 가져올 때, 댓글을 계층형태로 가져오도록 `ArticleWithCommentsResponse`에 `organizeChildComments` 추가.

--- src/main/java/com/fastcampus/projectboard/dto/ArticleCommentDto.java
+++ src/main/java/com/fastcampus/projectboard/dto/ArticleCommentDto.java
@@ -10,6 +10,7 @@ public record ArticleCommentDto(
         Long id,
         Long articleId,
         UserAccountDto userAccountDto,
+        Long parentCommentId,
         String content,
         LocalDateTime createdAt,
         String createdBy,
@@ -17,12 +18,20 @@ public record ArticleCommentDto(
         String modifiedBy
 ) {
 
+    public static ArticleCommentDto of(Long id, Long articleId, UserAccountDto userAccountDto, Long parentCommentId, String content, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleCommentDto(id, articleId, userAccountDto, parentCommentId, content, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
     public static ArticleCommentDto of(Long id, Long articleId, UserAccountDto userAccountDto, String content, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
-        return new ArticleCommentDto(id, articleId, userAccountDto, content, createdAt, createdBy, modifiedAt, modifiedBy);
+        return ArticleCommentDto.of(id, articleId, userAccountDto, null, content, createdAt, createdBy, modifiedAt, modifiedBy);
     }
 
     public static ArticleCommentDto of(Long articleId, UserAccountDto userAccountDto, String content) {
-        return new ArticleCommentDto(null, articleId, userAccountDto, content, null, null, null, null);
+        return ArticleCommentDto.of(null, articleId, userAccountDto, null, content, null, null, null, null);
+    }
+
+    public static ArticleCommentDto of(Long articleId, UserAccountDto userAccountDto, Long parentCommentId, String content) {
+        return ArticleCommentDto.of(null, articleId, userAccountDto, parentCommentId, content, null, null, null, null);
     }
 
     public static ArticleCommentDto from(ArticleComment entity) {
@@ -30,6 +39,7 @@ public record ArticleCommentDto(
                 entity.getId(),
                 entity.getArticle().getId(),
                 UserAccountDto.from(entity.getUserAccount()),
+                entity.getParentCommentId(),
                 entity.getContent(),
                 entity.getCreatedAt(),
                 entity.getCreatedBy(),
@@ -42,6 +52,7 @@ public record ArticleCommentDto(
         return ArticleComment.of(
                 entity,
                 userAccount,
+                parentCommentId,
                 content
         );
     }
